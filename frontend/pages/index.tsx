7ead27015c8c768840b8c719e6433ea2bb3085c2
Paulo <paulo@btoolsb.com>
2022-01-05 19:38:38 -0300

    Adding letter and remove from first attempt - Create new states to hold guess word, atempts and attempt letters - Create logic to add letters and removes - Starting logic to attempt a guess (WIP) - Keyboard functionality implemented

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,93 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import Keyboard from "../components/Keyboard";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [guessWordLetters, setGuessWordLetters] = useState<string[]>(
+    "carro".split("")
+  );
+  const [wordLength, setWordLength] = useState<number>(5);
+  const [maxWordAttempt, setMaxWordAttempt] = useState<number>(6);
+  const [wordAttempt, setWordAttempt] = useState<number>(0);
+  const [posLetter, setPostLetter] = useState<number>(0);
+
+  const initialWordsState = [];
+  for (let index = 0; index < maxWordAttempt; index++) {
+    const word = [];
+    for (let j = 0; j < wordLength; j++) {
+      word.push("");
+    }
+    initialWordsState.push(word);
+  }
+  const [words, setWords] = useState<string[][]>(initialWordsState);
+
+  const incrementPosLetter = () => {
+    const newPos = posLetter + 1;
+    if (newPos <= wordLength) {
+      setPostLetter(newPos);
+    }
+  };
+
+  const decrementPosLetter = () => {
+    const newPos = posLetter - 1;
+    if (newPos >= 0) {
+      setPostLetter(newPos);
+    }
+  };
+
+  const addLetter = (letter: string) => {
+    const newWordsState = [...words];
+    const currentWord = newWordsState[wordAttempt];
+    if (posLetter < wordLength) {
+      currentWord[posLetter] = letter;
+      setWords(newWordsState);
+      incrementPosLetter();
+    }
+  };
+
+  const deleteLastLetter = () => {
+    const newWordsState = [...words];
+    const currentWord = newWordsState[wordAttempt];
+    const newPosLetter = posLetter - 1;
+    if (newPosLetter >= 0) {
+      currentWord[newPosLetter] = "";
+      setWords(newWordsState);
+      decrementPosLetter();
+    }
+  };
+
+  const isValidWord = (word: string[]) => {
+    return true;
+  };
+
+  const incrementWordAttempt = () => {
+    const newWordAttempt = wordAttempt + 1;
+    if (newWordAttempt < maxWordAttempt) {
+      setWordAttempt(newWordAttempt);
+    }
+  };
+
+  const checkWord = (letters: string[]) => {
+    const word = letters.join();
+    const guessWord = guessWordLetters.join();
+    if (word === guessWord) {
+    } else {
+    }
+  };
+
+  const sendCurrentWord = () => {
+    const currentWord = words[wordAttempt];
+    if (currentWord.length != posLetter) {
+      alert("Faltando Letras");
+      return false;
+    }
+    if (!isValidWord(currentWord)) {
+      alert("Palavra não existe");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,9 +99,23 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>PALAVRAS</h1>
 
-        <div className={styles.grid}>Test</div>
-
-        <Keyboard />
+        {words.map((word, index) => {
+          const word_html = word.map((letter, index) => (
+            <p key={index} className={styles.card}>
+              {letter}
+            </p>
+          ));
+          return (
+            <div key={index} className={styles.grid}>
+              {word_html}
+            </div>
+          );
+        })}
+        <Keyboard
+          addLetter={addLetter}
+          deleteLastLetter={deleteLastLetter}
+          sendCurrentWord={sendCurrentWord}
+        />
       </main>
 
       <footer className={styles.footer}></footer>
